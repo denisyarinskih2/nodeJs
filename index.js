f747c1fe98abb904b7f3bbe9df9b25f6cd3a581c
denisyarinskih <denisyarinskih@gmail.com>
2024-02-22 22:12:52 +0100

    project init

---
+++ index.js
@@ -0,0 +1,8 @@
+function getRandomNumber() {
+    // console.log(Math.floor(Math.random() * 1000) + 1) to see the result in log
+    return Math.floor(Math.random() * 1000) + 1;
+}
+
+getRandomNumber();
+
+export default getRandomNumber;
